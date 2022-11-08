b65c2ab24f92e4cb51f71f60959c73353f8f6d39
Henry <henry@MacBook-Pro-de-Groupe.local>
2022-11-08 16:41:28 +0100

    correction of css file source on layout.php

--- view/layout.php
+++ view/layout.php
@@ -13,8 +13,7 @@
         <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="../assets/css/" rel="stylesheet" />
-        <link href="./styles/login.css" rel="stylesheet" />
+        <link href="../assets/css/style.css" rel="stylesheet" />
 
     </head>
     <body id="page-top">
