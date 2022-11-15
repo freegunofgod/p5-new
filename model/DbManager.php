bac7bdd5d92b0c0211c9e5113bc1dc53bc4facd1
Henry <henry@MacBook-Pro-de-Groupe.local>
2022-11-15 14:24:49 +0100

    Adding DbManager.php to UserManager and declare CRUD function for user

--- model/DbManager.php
+++ model/DbManager.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class Manager{
+abstract class DbManager{
 
     private $bdd;
 
--- model/UserManager.php
+++ model/UserManager.php
@@ -1,13 +1,21 @@
 <?php
 
-class UserManager {
+require_once('DbManager.php');
+
+class UserManager extends DbManager {
     public function getUser(){
-        $user = [
-            'login' => 'root',
-            'password' => 'root',
-            'email' => 'user@email.fr',
-        ];
-        
-        return $user;
+       //get a new user
+    }
+
+    public function createUser(){
+        //create a new user
+    }
+
+    public function deleteUser(){
+        //delete an user
+    }
+
+    public function updateUser(){
+        //update user information
     }
 }
