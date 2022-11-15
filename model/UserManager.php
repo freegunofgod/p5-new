d400abea9e80957ebe3a86f11ac7a77e35bd27c1
Henry <henry@MacBook-Pro-de-Groupe.local>
2022-11-15 16:50:37 +0100

    adding email verification to createUser

--- model/UserManager.php
+++ model/UserManager.php
@@ -17,20 +17,25 @@ class UserManager extends DbManager {
 
         $sql = $this->dbConnect()->prepare("SELECT * FROM users WHERE login = ?");
         $sql->execute(array($login));
-        $rowCountSql = $sql->rowCount();
+        $rowCountSqlLogin = $sql->rowCount();
 
-        if ($rowCountSql == 0) {
+        $sql = $this->dbConnect()->prepare("SELECT * FROM users WHERE email = ?");
+        $sql->execute(array($email));
+        $rowCountSqlEmail = $sql->rowCount();
 
+        if ($rowCountSqlEmail != 0){
+            $error = 'Cette email est déjà pris :(';
+            require('./view/createAccountPage.php');
+        }elseif($rowCountSqlLogin != 0) {
+            $error = 'Ce nom d\'utilisateur est déjà pris :(';
+            require('./view/createAccountPage.php');
+        }else{
             //Get connect with database //SQL search
             $sql = $this->Dbconnect()->prepare("INSERT INTO users (login,password,email) values(?, ?, ?)");
             //Execture Query
             $sql->execute(array($login,$password,$email));
 
             $success = 'Votre compte a bien été crée :)';
-            require('./view/createAccountPage.php');
-
-        } else {
-            $error = 'Ce nom d\'utilisateur est déjà pris :(';
             require('./view/createAccountPage.php');
         }
     }
