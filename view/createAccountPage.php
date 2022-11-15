45120aa366283cdb3b0aed4cd10ca1abe4caec7b
Henry <henry@MacBook-Pro-de-Groupe.local>
2022-11-15 15:08:12 +0100

    adapting template createAccountPage to Model logic

--- view/createAccountPage.php
+++ view/createAccountPage.php
@@ -2,7 +2,9 @@
 
 $title = "Account Creation Page";
 
-ob_start();?>
+ob_start();
+
+?>
 
 <section class="gradient-custom">
   <div class="container py-5 h-100">
@@ -16,28 +18,32 @@ ob_start();?>
               <h2 class="fw-bold mb-2 text-uppercase">Create your account</h2>
               <p class="text-white-50 mb-5">Please fill all input to create your account</p>
 
-              <div class="form-outline form-white mb-4">
-                <label class="form-label" for="typeIdentifiantX">Identifiant</label>
-                <input type="identifiant" id="typeIdentifiantX" class="form-control form-control-lg my-2" />
-              </div>
+              <form action="#" method="POST">
 
-              <div class="form-outline form-white mb-4">
-                <label class="form-label" for="typeEmailX">Email</label>
-                <input type="email" id="typeEmailX" class="form-control form-control-lg my-2" />
-              </div>
+                  <div class="form-outline form-white mb-4">
+                    <label class="form-label" for="typeIdentifiantX">Identifiant</label>
+                    <input type="identifiant" id="typeIdentifiantX" name="login" class="form-control form-control-lg my-2" />
+                  </div>
 
-              <div class="form-outline form-white mb-4">
-                <label class="form-label" for="typePasswordX">Password</label>
-                <input type="password" id="typePasswordX" class="form-control form-control-lg my-2" />
-              </div>
-              <p class=" small my-5 pb-lg-2 text-center"><button class="btn btn-outline-light btn-lg px-5" type="submit">Create your account</button></p>
-            </div>
+                  <div class="form-outline form-white mb-4">
+                    <label class="form-label" for="typeEmailX">Email</label>
+                    <input type="email" id="typeEmailX" name="email" class="form-control form-control-lg my-2" />
+                  </div>
+
+                  <div class="form-outline form-white mb-4">
+                    <label class="form-label" for="typePasswordX">Password</label>
+                    <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg my-2" />
+                  </div>
+
+                  <p class=" small my-5 pb-lg-2 text-center"><button class="btn btn-outline-light btn-lg px-5" type="submit">Create your account</button></p>
+               
+              </form>
+              
 
             <div>
               <p class="mb-0 text-center">Already have an account? <a href="index.php?action=login" class="text-white-50 fw-bold">Sign in</a>
               </p>
             </div>
-
           </div>
         </div>
       </div>
